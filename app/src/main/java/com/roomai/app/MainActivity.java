169d7c99edc5e2ebbac4f6060b48e65664754d21
Tarek <  GitHub>
2026-08-15 15:54:05 +0100

    Fix AlertDialog import

--- app/src/main/java/com/roomai/app/MainActivity.java
+++ app/src/main/java/com/roomai/app/MainActivity.java
@@ -2,6 +2,7 @@ package com.roomai.app;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
